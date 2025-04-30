13375a13de72b830c78866f9536200b9ee8b0a2b
Miguel Palma <mapalmalemus@gmail.com>
2025-04-30 08:16:33 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PIC12F683_BareMetal_Blink.X
+PIC12F683 protoboard Blink
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PIC12F683_BareMetal_Blink.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PIC12F683_BareMetal_Blink.X.production.hex
